c3576133f5d260dd9fe6c52f7799b6d3fc6d9414
4leaf.ysh <4leaf.ysh@gmail.com>
2020-05-14 16:20:05 +0900

    a

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
 app.listen("5001", () => {
   console.log("SERVER START 5001");
 });
